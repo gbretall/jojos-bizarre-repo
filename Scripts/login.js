70c88ed3b649986975cafb6b1a87d7439fbdd638
gbretall <gbretall@uci.edu>
2015-04-16 14:51:28 -0700

    Mine adds script loading (but it doesn't work currently)

---
+++ Scripts/login.js
@@ -0,0 +1,8 @@
+document.getElementById("loginButton").onclick = function() {loginToDB()};
+function loginToDB()
+{
+	var username = document.getElementsByName("Username");
+	var pass = document.getElementsByName("Password");
+	console.log(username);	
+}
+
