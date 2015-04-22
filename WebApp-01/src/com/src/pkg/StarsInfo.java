89c7158377339a92107606fedcab76bcb3f90696
gbretall <gbretall@uci.edu>
2015-04-22 13:03:32 -0700

    This should hopefully be the push that allows the basic search functionality to work

--- WebApp-01/src/com/src/pkg/StarsInfo.java
+++ WebApp-01/src/com/src/pkg/StarsInfo.java
@@ -4,14 +4,12 @@ public class StarsInfo {
 	int id;
 	String first_name;
 	String last_name;
-	String photo_url;
 	
-	public StarsInfo(int id, String first_name, String last_name, String photo_url)
+	public StarsInfo(int id, String first_name, String last_name)
 	{
 		this.id = id;
 		this.first_name = first_name;
 		this.last_name = last_name;
-		this.photo_url = photo_url;
 	}
 	
 }
